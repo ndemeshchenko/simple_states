61ed6d60ac8d830da46d09550ae1269b9d415d3f
agny <agny@macbook.local>
2011-12-23 05:01:52 +0200

    add callbacks

--- lib/simple_states/event.rb
+++ lib/simple_states/event.rb
@@ -17,35 +17,46 @@ module SimpleStates
     def call(object, *args)
       return if skip?(object, args)
 
-      run_callback(before, object, args) if options.before?
+      run_callback(:before, object, args)
       assert_transition(object)
       
-      result = yield if object.class.method_defined?(name)
-
-      set_state(object) if options.to
-      run_callback(options.after) if options.after?
- 
-      result
+      yield.tap do
+        set_state(object)
+        run_callback(:after, object, args)
+      end      
     end
 
     protected
     
     def skip?(object, args)
-			object.send method, *case arity = object.class.instance_method(method).arity
-				when 0; 	[]
-				when -1; 	[self].concat(args)
-				else;		  [self].concat(args).slice(0..arity - 1)
-    	end
+      result = false
+      result ||= !send_method(options.if, object, args) if options.if?
+      result ||= send_method(options.except, object, args) if options.except?
+      result
   	end
-		alias :run_callback :send_method
+  	
+  	def run_callback(type, object, args)
+	    sent_method(options.send(type), object, args) if options.send(type)
+	  end
 
 		def assert_transition(object)
 #			assert transition is allowed
 		end
 
-		def set_state(object)		
-			object.past_states << object.state
-			object.state = options.to		
+		def set_state(object)
+			if options.to
+			  object.past_states << object.state
+			  object.state = options.to
+		  end
 		end
+		
+		def send_method(method, object, args)
+      object.send method, *case arity = object.class.instance_method(method).arity
+	      when  0; []
+	      when -1; [self].concat(args)
+	      else;    [self].concat(args).slice(0..arity - 1)    
+	    end
+		end
+		
 	end
 end
--- test/cancellation_test.rb
+++ test/cancellation_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class SimpleStatesTest < Test::Unit::TestCase
+class CancellationTest < Test::Unit::TestCase
   include ClassCreateHelper
 
   test "Class.event wraps a method with cancellation callbacks (if: arity 0)" do
@@ -41,10 +41,36 @@ class SimpleStatesTest < Test::Unit::TestCase
     assert_equal 3, object.instance_variable_get(:@received_args).size
   end
 
-  test "Class.event wraps a method with cancellation callbacks (except)" do
+  test "Class.event wraps a method with cancellation callbacks (except: arity 0)" do
     klass = create_class do
       event :start, :from => :created, :to => :started, :except => :cancel?
       define_method(:cancel?) { true }
+    end
+
+    object = klass.new
+    object.start(:foo, :bar)
+
+    assert !object.started?
+    assert_equal :created, object.state
+  end
+
+  test "Class.event wraps a method with cancellation callbacks (except: arity 1)" do
+    klass = create_class do
+      event :start, :from => :created, :to => :started, :except => :cancel?
+      define_method(:cancel?) { |arg| @received_arg = arg; true }
+    end
+
+    object = klass.new
+    object.start(:foo, :bar)
+
+    assert !object.started?
+    assert_equal :created, object.state
+  end
+
+  test "Class.event wraps a method with cancellation callbacks (except: arity -1)" do
+    klass = create_class do
+      event :start, :from => :created, :to => :started, :except => :cancel?
+      define_method(:cancel?) { |*args| @received_args = args; true }
     end
 
     object = klass.new
