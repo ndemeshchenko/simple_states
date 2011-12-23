5166136a4bff45b3f1b8945f0d985cbcfd6194bf
Nikita Demeshchenko <neveragny@gmail.com>
2011-12-23 05:08:56 +0200

    add transition validation

--- lib/simple_states/event.rb
+++ lib/simple_states/event.rb
@@ -4,12 +4,12 @@ require 'hashr'
 module SimpleStates
   class Event
     attr_reader :name, :options
-    
-    def initializer(name, options = {})
-      @name = name
-      @options = Hash.new(options) do
+
+    def initialize(name, options = {})
+      @name    = name
+      @options = Hashr.new(options) do
         def except
-          self[:exept]
+          self[:except]
         end
       end
     end
@@ -17,46 +17,47 @@ module SimpleStates
     def call(object, *args)
       return if skip?(object, args)
 
+      assert_valid_transition(object)
       run_callback(:before, object, args)
-      assert_transition(object)
-      
+
       yield.tap do
         set_state(object)
         run_callback(:after, object, args)
-      end      
+      end
     end
 
     protected
-    
-    def skip?(object, args)
-      result = false
-      result ||= !send_method(options.if, object, args) if options.if?
-      result ||= send_method(options.except, object, args) if options.except?
-      result
-  	end
-  	
-  	def run_callback(type, object, args)
-	    sent_method(options.send(type), object, args) if options.send(type)
-	  end
 
-		def assert_transition(object)
-#			assert transition is allowed
-		end
+      def skip?(object, args)
+        result = false
+        result ||= !send_method(options.if, object, args) if options.if?
+        result ||= send_method(options.except, object, args) if options.except?
+        result
+      end
 
-		def set_state(object)
-			if options.to
-			  object.past_states << object.state
-			  object.state = options.to
-		  end
-		end
-		
-		def send_method(method, object, args)
-      object.send method, *case arity = object.class.instance_method(method).arity
-	      when  0; []
-	      when -1; [self].concat(args)
-	      else;    [self].concat(args).slice(0..arity - 1)    
-	    end
-		end
-		
-	end
+      def run_callback(type, object, args)
+        send_method(options.send(type), object, args) if options.send(type)
+      end
+
+      def assert_valid_transition(object)
+        if options.from && options.from != object.state
+          raise TransitionException, "#{object.inspect} can not receive event #{name.inspect} while in state #{object.state.inspect}."
+        end
+      end
+
+      def set_state(object)
+        if options.to
+          object.past_states << object.state
+          object.state = options.to
+        end
+      end
+
+      def send_method(method, object, args)
+        object.send method, *case arity = object.class.instance_method(method).arity
+          when 0;  []
+          when -1; [self].concat(args)
+          else;    [self].concat(args).slice(0..arity - 1)
+        end
+      end
+  end
 end
