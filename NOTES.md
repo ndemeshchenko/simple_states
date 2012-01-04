98de7f14ad1992c81bcad7dcf0eaa275a97db6c1
Nikita Demeshchenko <neveragny@gmail.com>
2012-01-04 21:33:35 +0200

    add states(:initial => :foo) and assume :created as the default initial state. moved event definition to proxy module so it is cached in class

--- NOTES.md
+++ NOTES.md
@@ -1,3 +1,5 @@
+add states :foo, :bar, :initial => :foo
+
 automatically touch attributes like [state]\at ?
 
 move event definition to a proxy modulewhich can be cached per class
--- test/class_macro_test.rb
+++ test/class_macro_test.rb
@@ -1,18 +1,27 @@
-require 'test_helper'
+  require 'test_helper'
 
-class ClassMacroTest < Test::Unit::TestCase
-  include ClassCreateHelper
+  class ClassMacroTest < Test::Unit::TestCase
+    include ClassCreateHelper
 
-  test "Class.states announces states the class supports" do
-    klass = create_class { states :created, :started, :completed }
-    assert_equal [:created, :started, :completed], klass.states
+    test "assumes :created as default initial state" do
+      assert_equal :created, create_class.initial_state
+    end
+
+    test "states announces states the class supports" do
+      klass = create_class { states :created, :started, :completed }
+      assert_equal [:created, :started, :completed], klass.states
+    end
+
+    test "states can set an initial state" do
+      klass = create_class { states :initial => :started }
+      assert_equal :started, klass.initial_state
+    end
+
+    test "event wraps a method with state progression" do
+      object = create_class { event :start, :from => :created, :to => :started }.new
+      object.start
+
+      assert object.started?
+      assert_equal :started, object.state
+    end
   end
-
-  test "Class.event wraps a method with state progression" do
-    object = create_class { event :start, :from => :created, :to => :started }.new
-    object.start
-
-    assert object.started?
-    assert_equal :started, object.state
-  end
-end
